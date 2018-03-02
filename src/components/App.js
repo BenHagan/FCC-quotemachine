d91cf4d33372bdfbd830abcadce231bd15ea7662
Ben Hagan <benhagan@users.noreply.github.com>
2018-03-01 22:03:46 -0600

    Changes to pass FCC unit tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import urlencode from 'urlencode';
 
 class App extends Component {
   componentDidMount() {
     this.props.fetchQuote();
   }
   render() {
+    let tweet = null;
+    const baseURL = 'https://twitter.com/intent/tweet?text=';
+    if (this.props.quote.quoteText) {
+      tweet = urlencode(
+        `${this.props.quote.quoteText} --${this.props.quote.quoteAuthor}`
+      );
+    }
     return (
       <div className="container-fluid">
         <h1 className="text-left">
@@ -15,22 +23,33 @@ class App extends Component {
         <div className="col-xs-2" />
         <div className="col-xs-8">
           <div className="row">
-            <div className="quote">{this.props.quote.quoteText}</div>
-            <div className="quote">--{this.props.quote.quoteAuthor}</div>
-          </div>
-          <div className="row buttons">
-            <button
-              onClick={() => {
-                this.props.fetchQuote();
-              }}
-              type="button"
-              className="btn btn-lg btn-default"
-            >
-              Refresh Quote
-            </button>
-            <button id="tweetQuote" className="btn btn-lg btn-info">
-              Tweet Quote
-            </button>
+            <div id="quote-box">
+              <div id="text" className="quote">
+                {this.props.quote.quoteText}
+              </div>
+              <div id="author" className="quote">
+                --{this.props.quote.quoteAuthor}
+              </div>
+              <div className="row buttons">
+                <button
+                  onClick={() => {
+                    this.props.fetchQuote();
+                  }}
+                  type="button"
+                  className="btn btn-lg btn-default"
+                  id="new-quote"
+                >
+                  Refresh Quote
+                </button>
+                <a
+                  id="tweet-quote"
+                  className="btn btn-lg btn-info"
+                  href={`${baseURL}${tweet}`}
+                >
+                  Tweet Quote
+                </a>
+              </div>
+            </div>
           </div>
         </div>
         <div className="col-xs-2" />
--- src/reducers/quote_reducer.js
+++ src/reducers/quote_reducer.js
@@ -1,7 +1,6 @@
 import { FETCH_QUOTE } from '../actions/types';
 
 export default function(state = '', action) {
-  debugger;
   switch (action.type) {
     case FETCH_QUOTE:
       return action.payload;
